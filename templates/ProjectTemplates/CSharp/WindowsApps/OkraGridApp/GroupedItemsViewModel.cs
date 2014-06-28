adc2186622941a8081ffb9402e202b5f264c81eb
Andrew Wilkinson <AndyWilkinsonNews@gmail.com>
2014-06-28 14:02:15 +0100

    Add shared behaviors for Visual Studio project templates

--- templates/ProjectTemplates/CSharp/WindowsApps/OkraGridApp/GroupedItemsViewModel.cs
+++ templates/ProjectTemplates/CSharp/WindowsApps/OkraGridApp/GroupedItemsViewModel.cs
@@ -3,6 +3,7 @@ using $safeprojectname$.Data;
 using Okra.Navigation;
 using System;
 using System.Collections.Generic;
+using System.Composition;
 using System.IO;
 using System.Linq;
 using Windows.Foundation;
@@ -18,50 +19,36 @@ namespace $safeprojectname$
     [ViewModelExport("Home")]
     public sealed partial class GroupedItemsViewModel : ViewModelBase
     {
-        //private NavigationHelper navigationHelper;
-        //private ObservableDictionary defaultViewModel = new ObservableDictionary();
+        private IEnumerable<SampleDataGroup> groups;
 
-        ///// <summary>
-        ///// NavigationHelper is used on each page to aid in navigation and 
-        ///// process lifetime management
-        ///// </summary>
-        //public NavigationHelper NavigationHelper
-        //{
-        //    get { return this.navigationHelper; }
-        //}
+        [ImportingConstructor]
+        public GroupedItemsViewModel(INavigationContext navigationContext)
+            : base(navigationContext)
+        {
+            Initialize();
+        }
 
-        ///// <summary>
-        ///// This can be changed to a strongly typed view model.
-        ///// </summary>
-        //public ObservableDictionary DefaultViewModel
-        //{
-        //    get { return this.defaultViewModel; }
-        //}
+        public IEnumerable<SampleDataGroup> Groups
+        {
+            get
+            {
+                return groups;
+            }
+            protected set
+            {
+                SetProperty(ref groups, value);
+            }
+        }
 
-        //public GroupedItemsPage()
-        //{
-        //    this.InitializeComponent();
-        //    this.navigationHelper = new NavigationHelper(this);
-        //    this.navigationHelper.LoadState += navigationHelper_LoadState;
-        //}
-
-        ///// <summary>
-        ///// Populates the page with content passed during navigation.  Any saved state is also
-        ///// provided when recreating a page from a prior session.
-        ///// </summary>
-        ///// <param name="sender">
-        ///// The source of the event; typically <see cref="NavigationHelper"/>
-        ///// </param>
-        ///// <param name="e">Event data that provides both the navigation parameter passed to
-        ///// <see cref="Frame.Navigate(Type, Object)"/> when this page was initially requested and
-        ///// a dictionary of state preserved by this page during an earlier
-        ///// session.  The state will be null the first time a page is visited.</param>
-        //private async void navigationHelper_LoadState(object sender, LoadStateEventArgs e)
-        //{
-        //    // TODO: Create an appropriate data model for your problem domain to replace the sample data
-        //    var sampleDataGroups = await SampleDataSource.GetGroupsAsync();
-        //    this.DefaultViewModel["Groups"] = sampleDataGroups;
-        //}
+        /// <summary>
+        /// Initializes the page with content.
+        /// </summary>
+        private async void Initialize()
+        {
+            // TODO: Create an appropriate data model for your problem domain to replace the sample data
+            var sampleDataGroups = await SampleDataSource.GetGroupsAsync();
+            this.Groups = sampleDataGroups;
+        }
 
         ///// <summary>
         ///// Invoked when a group header is clicked.
