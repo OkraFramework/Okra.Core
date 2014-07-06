36602013c2acf7c98969ea02d0233ffff5c3a4c5
Andrew Wilkinson <AndyWilkinsonNews@gmail.com>
2014-07-06 21:07:20 +0100

    Update build script for pushing packages to NuGet.org

--- scripts/Push.ps1
+++ scripts/Push.ps1
@@ -1,7 +1,28 @@
-param([Parameter(Mandatory=$true)][string]$Version) 
+[CmdletBinding()]
+Param([Parameter(Mandatory=$True)][Version]$versionNumber)
 
-cd ..\.nuget
-.\NuGet.exe push ..\artifacts\Okra.Core.$Version.nupkg
-.\NuGet.exe push ..\artifacts\Okra.MEF.$Version.nupkg
+# Move to the project root folder (parent from current script folder)
 
-$x = $host.UI.RawUI.ReadKey("NoEcho,IncludeKeyDown")
+function Get-ScriptDirectory
+{ 
+    $Invocation = (Get-Variable MyInvocation -Scope 1).Value 
+    Split-Path $Invocation.MyCommand.Path 
+}
+
+$rootFolder = (Get-Item (Get-ScriptDirectory)).Parent.FullName
+Set-Location $rootFolder
+
+# Check NuGet is installed and updated
+
+If (!(Test-Path .\.nuget\nuget.exe))
+{
+    New-Item .\.nuget -type directory -Force | Out-Null
+    Invoke-WebRequest 'https://www.nuget.org/nuget.exe' -OutFile '.\.nuget\nuget.exe'
+}
+
+.\.nuget\NuGet.exe update -self
+
+# Push packages to NuGet.org
+
+.\.nuget\NuGet.exe push .\artifacts\Okra.Core.$versionNumber.nupkg
+.\.nuget\NuGet.exe push .\artifacts\Okra.MEF.$versionNumber.nupkg
