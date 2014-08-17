0fdb5224167252f4ec72a5220cd50c8c37e6060f
Andrew Wilkinson <AndyWilkinsonNews@gmail.com>
2014-08-17 17:16:27 +0100

    Add keyboard navigation within navigation stack. Fixes #16.

--- src/Okra.Core/Okra.Core.Shared/Navigation/WindowNavigationTarget.cs
+++ src/Okra.Core/Okra.Core.Shared/Navigation/WindowNavigationTarget.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.System;
+using Windows.UI.Core;
 using Windows.UI.Xaml;
 using Windows.UI.Xaml.Controls;
 
@@ -12,16 +14,34 @@ namespace Okra.Navigation
     {
         // *** Fields ***
 
+        private bool eventHandlersRegistered;
         private ContentControl contentHost;
+        private INavigationBase navigationManager;
 
         // *** Methods ***
 
         public void NavigateTo(object page, INavigationBase navigationManager)
         {
+            this.navigationManager = navigationManager;
+
+            if (!eventHandlersRegistered)
+            {
+                RegisterEventHandlers();
+                eventHandlersRegistered = true;
+            }
+
             SetWindowContent(page);
         }
 
         // *** Protected Methods ***
+
+        protected virtual void RegisterEventHandlers()
+        {
+#if WINDOWS_APP
+            Window.Current.CoreWindow.Dispatcher.AcceleratorKeyActivated += Window_AcceleratorKeyActivated;
+            Window.Current.CoreWindow.PointerPressed += Window_PointerPressed;
+#endif
+        }
 
         protected virtual void SetWindowContent(object page)
         {
@@ -44,6 +64,53 @@ namespace Okra.Navigation
             // Set the content to display
 
             contentHost.Content = page;
+        }
+
+        // *** Event Handlers ***
+
+#if WINDOWS_APP
+
+        protected void Window_AcceleratorKeyActivated(CoreDispatcher sender, AcceleratorKeyEventArgs args)
+        {
+            VirtualKey key = args.VirtualKey;
+            CoreAcceleratorKeyEventType eventType = args.EventType;
+
+            // Only continue processing if we are pressing the left, right, back or forward keys
+
+            if ((eventType == CoreAcceleratorKeyEventType.KeyDown || eventType == CoreAcceleratorKeyEventType.SystemKeyDown) &&
+                (key == VirtualKey.Left || key == VirtualKey.Right || key == VirtualKey.GoBack || key == VirtualKey.GoForward))
+            {
+                // Get the current modifier state
+
+                CoreWindow coreWindow = Window.Current.CoreWindow;
+                bool menuKey = (coreWindow.GetKeyState(VirtualKey.Menu) & CoreVirtualKeyStates.Down) == CoreVirtualKeyStates.Down;
+                bool controlKey = (coreWindow.GetKeyState(VirtualKey.Control) & CoreVirtualKeyStates.Down) == CoreVirtualKeyStates.Down;
+                bool shiftKey = (coreWindow.GetKeyState(VirtualKey.Shift) & CoreVirtualKeyStates.Down) == CoreVirtualKeyStates.Down;
+                bool noModifiers = !menuKey && !controlKey && !shiftKey;
+                bool onlyAlt = menuKey && !controlKey && !shiftKey;
+
+                // Handle going back via keyboard (Alt+Left or GoBack)
+
+                if ((key == VirtualKey.Left && onlyAlt) || (key== VirtualKey.GoBack && noModifiers))
+                {
+                    args.Handled = true;
+                    GoBack();
+                }
+            }
+        }
+
+        protected void Window_PointerPressed(CoreWindow sender, PointerEventArgs args)
+        {
+        }
+
+#endif
+
+        // *** Private Methods ***
+
+        private void GoBack()
+        {
+            if (navigationManager != null && navigationManager.NavigationStack.CanGoBack)
+                navigationManager.NavigationStack.GoBack();
         }
     }
 }
--- test/Okra.Core.Tests/Okra.Core.Tests.Shared/Navigation/WindowNavigationTargetFixture.cs
+++ test/Okra.Core.Tests/Okra.Core.Tests.Shared/Navigation/WindowNavigationTargetFixture.cs
@@ -4,6 +4,7 @@ using Okra.Tests.Mocks;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using Windows.UI.Core;
 
 namespace Okra.Tests.Navigation
 {
@@ -22,11 +23,46 @@ namespace Okra.Tests.Navigation
             CollectionAssert.AreEqual(new object[] { page }, target.SetWindowContentCalls);
         }
 
+        [TestMethod]
+        public void NavigateTo_FirstCallRegistersEventHandlers()
+        {
+            TestableWindowNavigationTarget target = new TestableWindowNavigationTarget();
+            MockNavigationBase navigationBase = new MockNavigationBase();
+            object page = new object();
+
+            Assert.AreEqual(0, target.RegisterEventHandlersCallCount);
+
+            target.NavigateTo(page, navigationBase);
+
+            Assert.AreEqual(1, target.RegisterEventHandlersCallCount);
+        }
+
+        [TestMethod]
+        public void NavigateTo_RegisterEventHandlersIsOnlyCalledOnce()
+        {
+            TestableWindowNavigationTarget target = new TestableWindowNavigationTarget();
+            MockNavigationBase navigationBase = new MockNavigationBase();
+            object page = new object();
+
+            Assert.AreEqual(0, target.RegisterEventHandlersCallCount);
+
+            target.NavigateTo(page, navigationBase);
+            target.NavigateTo(page, navigationBase);
+
+            Assert.AreEqual(1, target.RegisterEventHandlersCallCount);
+        }
+
         // *** Private classes ***
 
         public class TestableWindowNavigationTarget : WindowNavigationTarget
         {
+            public int RegisterEventHandlersCallCount = 0;
             public List<object> SetWindowContentCalls = new List<object>();
+
+            protected override void RegisterEventHandlers()
+            {
+                RegisterEventHandlersCallCount++;
+            }
 
             protected override void SetWindowContent(object page)
             {
