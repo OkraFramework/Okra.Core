a905337c9da4005c4ba0620fdb1d21b26a7782ff
Andrew Wilkinson <AndyWilkinsonNews@gmail.com>
2015-08-09 21:31:26 +0100

    Add generic NavigationStack<T> to the PortableCore

---
+++ src/Okra.Core/Okra.PortableCore.Shared/Navigation/NavigationStackOfT.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace Okra.Navigation
+{
+    public class NavigationStack<T> : IReadOnlyList<T>, INotifyPropertyChanged
+    {
+        // *** Fields ***
+
+        private readonly List<T> _internalStack = new List<T>();
+        private readonly List<T> _forwardStack = new List<T>();
+
+        // *** Events ***
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        // *** Properties ***
+
+        public T this[int index]
+        {
+            get
+            {
+                return _internalStack[index];
+            }
+        }
+
+        public bool CanGoBack => _internalStack.Count > 0;
+
+        public bool CanGoForward => _forwardStack.Count > 0;
+
+        public int Count => _internalStack.Count;
+
+        public T CurrentItem => _internalStack.Count == 0 ? default(T) : _internalStack[_internalStack.Count - 1];
+
+        // *** Methods ***
+
+        public void Clear()
+        {
+            _internalStack.Clear();
+            _forwardStack.Clear();
+        }
+
+        public void GoBack()
+        {
+            T item = Pop(_internalStack);
+            _forwardStack.Add(item);
+        }
+
+        public void GoForward()
+        {
+            T item = Pop(_forwardStack);
+            _internalStack.Add(item);
+        }
+
+        public void NavigateTo(T item)
+        {
+            _internalStack.Add(item);
+            _forwardStack.Clear();
+        }
+
+        // *** Enumerators ***
+
+        public IEnumerator<T> GetEnumerator() => _internalStack.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => _internalStack.GetEnumerator();
+
+        // *** Protected Methods ***
+
+        protected virtual void OnPropertyChanged(string propertyName) => PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+
+        // *** Private Methods ***
+
+        public T Pop(List<T> list)
+        {
+            int index = list.Count - 1;
+            T item = list[index];
+            list.RemoveAt(index);
+            return item;
+        }
+    }
+}
---
+++ test/Okra.Core.Tests/Okra.PortableCore.Tests.Shared/Navigation/NavigationStackOfTFixture.cs
@@ -0,0 +1,358 @@
+﻿using Okra.Navigation;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using Xunit;
+
+namespace Okra.Tests.Navigation
+{
+    public class NavigationStackOfTFixture
+    {
+        // *** Test Data ***
+
+        public static object[][] SingleTestValues = { new object[] { 42 },
+                                                      new object[] { "string" },
+                                                      new object[] { new TestClass("string") } };
+
+        public static object[][] DoubleTestValues = { new object[] { 42, 68 },
+                                                      new object[] { new TestClass("alpha"), new TestClass("beta") },
+                                                      new object[] { new TestClass("alpha"), new TestClass("beta") } };
+
+        // *** Tests ***
+
+        [Theory]
+        [MemberData(nameof(SingleTestValues))]
+        public void InitialStack_IsEmpty<T>(T defaultItem)
+        {
+            var stack = new NavigationStack<T>();
+
+            Assert.NotNull(stack);
+
+            Assert.Equal(0, stack.Count);
+            Assert.Equal(default(T), stack.CurrentItem);
+
+            Assert.False(stack.CanGoBack);
+            Assert.False(stack.CanGoForward);
+        }
+
+        [Theory]
+        [MemberData(nameof(DoubleTestValues))]
+        public void Clear_ClearsStackContent<T>(T item1, T item2)
+        {
+            var stack = new NavigationStack<T>();
+
+            stack.NavigateTo(item1);
+            stack.NavigateTo(item2);
+            stack.GoBack();
+            stack.Clear();
+
+            Assert.Equal(0, stack.Count);
+            Assert.Equal(default(T), stack.CurrentItem);
+
+            Assert.False(stack.CanGoBack);
+            Assert.False(stack.CanGoForward);
+        }
+
+        [Theory]
+        [MemberData(nameof(DoubleTestValues))]
+        public void GetEnumerator_Generic_EnumeratesPagesInStack<T>(T item1, T item2)
+        {
+            var stack = new NavigationStack<T>();
+
+            stack.NavigateTo(item1);
+            stack.NavigateTo(item2);
+
+            T[] items = ((IEnumerable<T>)stack).ToArray();
+
+            Assert.Equal(new T[] { item1, item2 }, items);
+        }
+
+        [Theory]
+        [MemberData(nameof(DoubleTestValues))]
+        public void GetEnumerator_NonGeneric_EnumeratesPagesInStack<T>(T item1, T item2)
+        {
+            var stack = new NavigationStack<T>();
+
+            stack.NavigateTo(item1);
+            stack.NavigateTo(item2);
+
+            var items = new List<T>();
+
+            foreach (T item in (IEnumerable)stack)
+                items.Add(item);
+
+            Assert.Equal(new T[] { item1, item2 }, items);
+        }
+
+        [Theory]
+        [MemberData(nameof(SingleTestValues))]
+        public void GoBack_ThrowsException_IfNoPagesInBackStack<T>(T item)
+        {
+            var stack = new NavigationStack<T>();
+
+            stack.NavigateTo(item);
+            stack.GoBack();
+
+            var e = Assert.Throws<InvalidOperationException>(() => stack.GoBack());
+            Assert.Equal("You cannot navigate backwards as the back stack is empty.", e.Message);
+        }
+
+        [Theory]
+        [MemberData(nameof(SingleTestValues))]
+        public void Navigate_ToPage_AddsOneItemToStack<T>(T item)
+        {
+            var stack = new NavigationStack<T>();
+
+            stack.NavigateTo(item);
+
+            Assert.Equal(1, stack.Count);
+            Assert.Equal(item, stack[0]);
+            Assert.Equal(item, stack.CurrentItem);
+
+            Assert.True(stack.CanGoBack);
+            Assert.False(stack.CanGoForward);
+        }
+
+        [Theory]
+        [MemberData(nameof(DoubleTestValues))]
+        public void Navigate_ToPage_ToPage_AddsTwoItemsToStack<T>(T item1, T item2)
+        {
+            var stack = new NavigationStack<T>();
+
+            stack.NavigateTo(item1);
+            stack.NavigateTo(item2);
+
+            Assert.Equal(2, stack.Count);
+            Assert.Equal(item1, stack[0]);
+            Assert.Equal(item2, stack[1]);
+            Assert.Equal(item2, stack.CurrentItem);
+
+            Assert.True(stack.CanGoBack);
+            Assert.False(stack.CanGoForward);
+        }
+
+        [Theory]
+        [MemberData(nameof(SingleTestValues))]
+        public void Navigate_ToPage_Back_GivesEmptyStack<T>(T item)
+        {
+            var stack = new NavigationStack<T>();
+
+            stack.NavigateTo(item);
+            stack.GoBack();
+
+            Assert.Equal(0, stack.Count);
+            Assert.Equal(default(T), stack.CurrentItem);
+
+            Assert.False(stack.CanGoBack);
+            Assert.True(stack.CanGoForward);
+        }
+
+        [Theory]
+        [MemberData(nameof(DoubleTestValues))]
+        public void Navigate_ToPage_ToPage_Back_LeavesOneItemOnStack<T>(T item1, T item2)
+        {
+            var stack = new NavigationStack<T>();
+
+            stack.NavigateTo(item1);
+            stack.NavigateTo(item2);
+            stack.GoBack();
+
+            Assert.Equal(1, stack.Count);
+            Assert.Equal(item1, stack[0]);
+            Assert.Equal(item1, stack.CurrentItem);
+
+            Assert.True(stack.CanGoBack);
+            Assert.True(stack.CanGoForward);
+        }
+
+        [Theory]
+        [MemberData(nameof(SingleTestValues))]
+        public void Navigate_ToPage_Back_Forward_AddsItemBackOntoStack<T>(T item)
+        {
+            var stack = new NavigationStack<T>();
+
+            stack.NavigateTo(item);
+            stack.GoBack();
+            stack.GoForward();
+
+            Assert.Equal(1, stack.Count);
+            Assert.Equal(item, stack[0]);
+            Assert.Equal(item, stack.CurrentItem);
+
+            Assert.True(stack.CanGoBack);
+            Assert.False(stack.CanGoForward);
+        }
+
+        [Theory]
+        [MemberData(nameof(DoubleTestValues))]
+        public void Navigate_ToPage_ToPage_Back_Forward_AddsItemBackOntoStack<T>(T item1, T item2)
+        {
+            var stack = new NavigationStack<T>();
+
+            stack.NavigateTo(item1);
+            stack.NavigateTo(item2);
+            stack.GoBack();
+            stack.GoForward();
+
+            Assert.Equal(2, stack.Count);
+            Assert.Equal(item1, stack[0]);
+            Assert.Equal(item2, stack[1]);
+            Assert.Equal(item2, stack.CurrentItem);
+
+            Assert.True(stack.CanGoBack);
+            Assert.False(stack.CanGoForward);
+        }
+
+        [Theory]
+        [MemberData(nameof(DoubleTestValues))]
+        public void Navigate_ToPage_ToPage_Back_Back_Forward_AddsItemBackOntoStack<T>(T item1, T item2)
+        {
+            var stack = new NavigationStack<T>();
+
+            stack.NavigateTo(item1);
+            stack.NavigateTo(item2);
+            stack.GoBack();
+            stack.GoBack();
+            stack.GoForward();
+
+            Assert.Equal(1, stack.Count);
+            Assert.Equal(item1, stack[0]);
+            Assert.Equal(item1, stack.CurrentItem);
+
+            Assert.True(stack.CanGoBack);
+            Assert.True(stack.CanGoForward);
+        }
+
+        [Theory]
+        [MemberData(nameof(DoubleTestValues))]
+        public void Navigate_ToPage_ToPage_Back_Back_Forward_Forward_AddsItemsBackOntoStack<T>(T item1, T item2)
+        {
+            var stack = new NavigationStack<T>();
+
+            stack.NavigateTo(item1);
+            stack.NavigateTo(item2);
+            stack.GoBack();
+            stack.GoBack();
+            stack.GoForward();
+            stack.GoForward();
+
+            Assert.Equal(2, stack.Count);
+            Assert.Equal(item1, stack[0]);
+            Assert.Equal(item2, stack[1]);
+            Assert.Equal(item2, stack.CurrentItem);
+
+            Assert.True(stack.CanGoBack);
+            Assert.False(stack.CanGoForward);
+        }
+
+        [Theory]
+        [MemberData(nameof(DoubleTestValues))]
+        public void Navigate_ToPage_Back_ToPage_LeavesOneItemOnStack<T>(T item1, T item2)
+        {
+            var stack = new NavigationStack<T>();
+
+            stack.NavigateTo(item1);
+            stack.GoBack();
+            stack.NavigateTo(item2);
+
+            Assert.Equal(1, stack.Count);
+            Assert.Equal(item2, stack[0]);
+            Assert.Equal(item2, stack.CurrentItem);
+
+            Assert.True(stack.CanGoBack);
+            Assert.False(stack.CanGoForward);
+        }
+
+        [Theory]
+        [MemberData(nameof(DoubleTestValues))]
+        public void Navigate_ToPage_Back_ToPage_Back_Forward_AddsCorrectItemOntoStack<T>(T item1, T item2)
+        {
+            var stack = new NavigationStack<T>();
+
+            stack.NavigateTo(item1);
+            stack.GoBack();
+            stack.NavigateTo(item2);
+            stack.GoBack();
+            stack.GoForward();
+
+            Assert.Equal(1, stack.Count);
+            Assert.Equal(item2, stack[0]);
+            Assert.Equal(item2, stack.CurrentItem);
+
+            Assert.True(stack.CanGoBack);
+            Assert.False(stack.CanGoForward);
+        }
+
+        [Theory]
+        [MemberData(nameof(SingleTestValues))]
+        public void PropertyChanged_IsRaised_WithNavigation_ToPage<T>(T item)
+        {
+            var stack = new NavigationStack<T>();
+
+            AssertPropertyChangedEvents(stack, () => stack.NavigateTo(item));
+        }
+
+        // *** Test Classes ***
+
+        public class TestClass
+        {
+            public TestClass(string name)
+            {
+                Name = name;
+            }
+
+            public string Name { get; }
+        }
+
+        // *** Private Methods ***
+
+        private void AssertPropertyChangedEvents<T>(T obj, Action action) where T : INotifyPropertyChanged
+        {
+            TypeInfo typeInfo = typeof(T).GetTypeInfo();
+
+            // Get current values for all properties
+
+            var initialProperties = new Dictionary<PropertyInfo, object>();
+
+            foreach (PropertyInfo property in typeInfo.DeclaredProperties)
+            {
+                if (property.GetIndexParameters().Length == 0)
+                    initialProperties[property] = property.GetValue(obj);
+            }
+
+            // Perform the action, recording property changed events
+
+            var changedPropertyNames = new List<string>();
+
+            PropertyChangedEventHandler eventHandler = (sender, e) =>
+            {
+                Assert.Equal(obj, sender);
+                changedPropertyNames.Add(e.PropertyName);
+            };
+
+            obj.PropertyChanged += eventHandler;
+            action();
+            obj.PropertyChanged -= eventHandler;
+
+            // Check that property changed events were raised for all modified properties
+
+            foreach (KeyValuePair<PropertyInfo, object> initialProperty in initialProperties)
+            {
+                if (initialProperty.Key.GetValue(obj) != initialProperty.Value)
+                {
+                    string propertyName = initialProperty.Key.Name;
+                    Assert.True(changedPropertyNames.Contains(propertyName), $"Property '{propertyName}' did not raise a PropertyChanged event");
+                    changedPropertyNames.Remove(propertyName);
+                }
+            }
+
+            // Check that no unchanged properties raised PropertyChanged events
+
+            Assert.True(changedPropertyNames.Count == 0, $"Unchanged Property '{changedPropertyNames[0]}' raised PropertyChanged event");
+        }
+    }
+}
