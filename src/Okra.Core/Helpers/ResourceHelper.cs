572bbc1395ff7eb8330cae6a55542ce8c27757fc
Andrew Wilkinson <AndyWilkinsonNews@gmail.com>
2016-05-21 17:34:42 +0100

    Extract Okra.MEF project code into separate projects

---
+++ src/Okra.Core/Helpers/ResourceHelper.cs
@@ -0,0 +1,14 @@
+﻿using Okra.Core;
+
+namespace Okra.Helpers
+{
+    internal static class ResourceHelper
+    {
+        // *** Methods ***
+
+        public static string GetErrorResource(string resourceName)
+        {
+            return Resources.ResourceManager.GetString(resourceName);
+        }
+    }
+}
---
+++ src/Okra.Core/Properties/AssemblyInfo.cs
@@ -0,0 +1,28 @@
+﻿using System.Reflection;
+using System.Resources;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("Okra App Framework (Portable Core)")]
+[assembly: AssemblyDescription("The Okra App Framework is an app centric MVVM framework. This package contains only the portable core assembly.")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Andrew Wilkinson")]
+[assembly: AssemblyProduct("Okra.Core")]
+[assembly: AssemblyCopyright("Copyright \u00A9 Andrew Wilkinson 2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: NeutralResourcesLanguage("en")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: AssemblyVersion("1.2.0.0")]
+[assembly: AssemblyFileVersion("1.2.0.0")]
+[assembly: AssemblyInformationalVersion("1.2.0")]
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("a7393a4a-b0d9-4aab-a3e5-0ab5eabf4dd3")]
