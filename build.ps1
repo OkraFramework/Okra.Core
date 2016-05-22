8d9d7012867e76d0743da931c1c4677c5c0449d7
Andrew Wilkinson <AndyWilkinsonNews@gmail.com>
2016-05-22 16:22:02 +0100

    Update build scripts to the Okra-Build shared build system

--- build.ps1
+++ build.ps1
@@ -1,2 +1,30 @@
-dotnet test ./test/Okra.Core.Tests/
-dotnet pack ./src/Okra.Core/ --output artifacts
+# Move to the project root folder (current script folder)
+ 
+function Get-ScriptDirectory 
+{  
+    $Invocation = (Get-Variable MyInvocation -Scope 1).Value  
+    Split-Path $Invocation.MyCommand.Path  
+}
+
+$rootFolder = (Get-Item (Get-ScriptDirectory)).FullName 
+Set-Location $rootFolder
+
+# Clone the Okra Build system (or use local copy if in parent folder)
+
+If (Test-Path "./.build")
+{
+    Remove-Item "./.build" -Recurse -Force
+}
+
+If (Test-Path "../Okra-Build")
+{
+    Copy-Item "../Okra-Build" "./.build" -Recurse
+}
+Else
+{
+    git clone https://github.com/OkraFramework/Okra-Build.git .build
+}
+
+# Run the build script
+
+.\.build\build.ps1 @args
